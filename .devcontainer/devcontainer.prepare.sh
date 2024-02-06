c607c7d0852e1e67d12cd3ce4f5aa280f7a80e15
Rodge Fu <wenjiefu@microsoft.com>
2024-02-06 21:53:24 +0800

    update readme

--- .devcontainer/devcontainer.prepare.sh
+++ .devcontainer/devcontainer.prepare.sh
@@ -1,4 +1,1 @@
-cd ~/workspaces/azure-sdk-for-net/tools/VsCodeExtensions/azure-sdk-mgmt-codegen-helper
-npm install
-vsce package --allow-missing-repository --out output.vsix
-code --install-extension output.vsix
+cd ~/workspaces/azure-sdk-for-net/tools/VsCodeExtensions/azure-sdk-mgmt-codegen-helper && npm install && vsce package --allow-missing-repository --out output.vsix && code --install-extension output.vsix
